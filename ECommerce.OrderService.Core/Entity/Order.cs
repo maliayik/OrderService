86d8b444e9bc78d122b400b4747d102a9c3f1691
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2025-06-28 14:48:56 +0300

    OrderService.Core & Infrastructure layers added

---
+++ ECommerce.OrderService.Core/Entity/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.OrderService.Core.Entity
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public ICollection<OrderItem>? Items { get; set; }
+    }
+}
---
+++ ECommerce.OrderService.Infrastructure/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.OrderService.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.OrderService.Infrastructure
+{
+    public class AppDbContext(DbContextOptions options):DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
